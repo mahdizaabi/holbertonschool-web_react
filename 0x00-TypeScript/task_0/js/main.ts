a1694ce9868bdf7ebc1edbbb422eb0aa42548033
mahdi <mahdi.zaabi@gmail.com>
2021-06-22 16:59:40 +0000

    add

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -1,49 +1,50 @@
-interface Student {
-    firstName: string;
-    lastName: string;
-    age: number;
-    location: string;
-  }
-  
-  const student1: Student = {
-    firstName: "Larry",
-    lastName: "Hudson",
-    age: 32,
-    location: "San Francisco",
-  };
-  
-  const student2: Student = {
-    firstName: "Karla",
-    lastName: "Dermond",
-    age: 26,
-    location: "New York",
-  };
-  
-  const studentsList: Array<Student> = [student1, student2];
-  
-  const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
-  
-  const table: HTMLTableElement = document.createElement("table");
-  const thead: HTMLTableSectionElement = document.createElement("thead");
-  const tbody: HTMLTableSectionElement = document.createElement("tbody");
-  
-  const rowHead: HTMLTableRowElement = thead.insertRow(0);
-  const cell1Head: HTMLTableCellElement = rowHead.insertCell(0);
-  const cell2Head: HTMLTableCellElement = rowHead.insertCell(1);
-  
-  cell1Head.innerHTML = "firstName";
-  cell2Head.innerHTML = "location";
-  
-  table.append(thead);
-  
-  studentsList.forEach((student) => {
-    const row: HTMLTableRowElement = tbody.insertRow(0);
-    const cell1: HTMLTableCellElement = row.insertCell(0);
-    const cell2: HTMLTableCellElement = row.insertCell(1);
-  
-    cell1.innerHTML = student.firstName;
-    cell2.innerHTML = student.location;
-  });
-  
-  table.append(tbody);
-  body.append(table);
+
+interface myInterface {
+    firstName:string,
+    lastName: string,
+    age:number,
+    location:string
+
+}
+
+const studentList: myInterface[] = [];
+const student1: myInterface = {
+    firstName: "st1",
+    lastName: "st11",
+    age: 1,
+    location:"std"
+}
+const student2: myInterface = {
+    firstName: "st2",
+    lastName: "st12",
+    age: 2,
+    location:"std"
+}
+
+const studentsList: Array<myInterface> = [student1, student2];
+
+const body = document.getElementsByTagName("body")[0];
+
+const tableComponent = document.createElement("table");
+tableComponent.innerHTML  += `<tr>
+<th>firstName</th>
+<th>location</th>
+</tr>`
+body.appendChild(tableComponent);
+
+const studentComponent = (student: myInterface):string => `<tr>
+<td>${student.firstName}</td>
+<td>${student.location}</td>
+</tr>`
+
+studentList.forEach((student: myInterface)=>{
+    tableComponent.innerHTML += studentComponent(student);
+})
+
+
+
+
+
+
+
+
