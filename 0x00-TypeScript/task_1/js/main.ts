bd83aa553a496e7a2dafc2bc93db3c9124fdce0d
mahdizaabi <mahdi.zaabi@gmail.com>
2021-06-23 01:14:54 +0200

    add

---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,10 @@
+interface Iteacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee:string;
+    yearsOfExperience?: number;
+    location: string;
+    [propName: string]: any;
+}
+
+
