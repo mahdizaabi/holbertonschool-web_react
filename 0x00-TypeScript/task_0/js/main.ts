0cb7d6bf28806397dfd0cd869b4ba3c5f6b2e70e
mahdizaabi <mahdi.zaabi@gmail.com>
2021-06-22 23:36:18 +0200

    add

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -26,12 +26,12 @@ const studentsList: Array<Istudent> = [student1, student2];
 
 
 function generateTableHead(table: HTMLTableElement,headers: string[]): void {
-    const thead = table.createTHead();
-    const row = thead.insertRow();
+    const thead: HTMLTableSectionElement  = table.createTHead();
+    const row: HTMLTableRowElement   = thead.insertRow();
 
     headers.forEach((headerTitle) =>{
-        const th = document.createElement("th");
-        const text = document.createTextNode(headerTitle);
+        const th: HTMLTableHeaderCellElement  = document.createElement("th");
+        const text: Text = document.createTextNode(headerTitle);
         th.appendChild(text);
         row.appendChild(th);
         thead.appendChild(row)
@@ -44,19 +44,19 @@ function generateTable(table: HTMLTableElement, data: Istudent[]): void{
     const tbody: HTMLTableSectionElement = document.createElement("tbody");
     table.appendChild(tbody);
     data.forEach((element: Istudent)=> {
-        const row = tbody.insertRow();
-        const cell1 = row.insertCell();
-        const text1 = document.createTextNode(element.firstName);
+        const row: HTMLTableRowElement = tbody.insertRow();
+        const cell1: HTMLTableDataCellElement = row.insertCell();
+        const text1: Text = document.createTextNode(element.firstName);
         cell1.appendChild(text1)
-        const cell2 = row.insertCell();
-        const text2 = document.createTextNode(element.location);
+        const cell2: HTMLTableDataCellElement = row.insertCell();
+        const text2: Text = document.createTextNode(element.location);
         cell2.appendChild(text2);
     });
 }
 
 
-const body = document.getElementsByTagName("body")[0];
-const table = document.createElement("table");
+const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
+const table: HTMLTableElement = document.createElement("table");
 body.appendChild(table)
 
 generateTableHead(table, headers);
