5c9db067d57193b8cfbfc797f1c14527a824c887
mahdizaabi <mahdi.zaabi@gmail.com>
2021-06-23 21:27:51 +0200

    add

--- 0x00-TypeScript/task_3/js/main.ts
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -1,7 +1,7 @@
 /// <reference path="./crud.d.ts" />
-import { RowID, RowElement  } from "./interface";
 
-import * as CRUD from './crud';
+import { RowID, RowElement  } from "./interface";
+import * as CRUD from './crud.js';
 
 const row: RowElement = {
     firstName: 'Guilaume',
