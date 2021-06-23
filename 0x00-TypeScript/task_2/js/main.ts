b9a403d2f0f782f9a2a565ed3d416d13115957aa
mahdizaabi <mahdi.zaabi@gmail.com>
2021-06-23 16:23:27 +0200

    add

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -4,13 +4,13 @@ interface DirectorInterface {
     workDirectorTasks(): string;
   }
   
-  interface TeacherInterface {
+interface TeacherInterface {
     workFromHome(): string;
     getCoffeeBreak(): string;
     workTeacherTasks(): string;
   }
   
-  export class Director implements DirectorInterface {
+export class Director implements DirectorInterface {
     workFromHome(): string {
       return "Working from home";
     }
@@ -40,3 +40,18 @@ interface DirectorInterface {
     if (typeof salary === "number" && salary < 500) return new Teacher();
     return new Director();
   }
+
+  export function isDirector(employee: (Director | Teacher)): employee is Director{
+        return (employee as Director) instanceof Director;
+  }
+
+  export function executeWork(employee: (Director | Teacher)){
+    if(isDirector(employee)){
+      return employee.workDirectorTasks();
+    }else {
+      return employee.workTeacherTasks();
+    }
+  }
+
+  console.log(executeWork(createEmployee(200)));
+  console.log(executeWork(createEmployee(1000)));
