7cec863d7b25bd7a4f3bf2be98c723423ee49c3a
mahdizaabi <mahdi.zaabi@gmail.com>
2021-06-23 11:30:12 +0200

    add

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -1,44 +1,43 @@
 interface DirectorInterface {
-    workFromHome():string;
-    getCoffeeBreak():string;
-    workDirectorTasks():string;
-}
-
-interface TeacherInterface {
-    workFromHome():string;
-    getCoffeeBreak():string;
-    workTeacherTasks():string;
-}
-
-class Director implements DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+  }
+  
+  interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+  }
+  
+  export class Director implements DirectorInterface {
     workFromHome(): string {
-        throw new Error("Working from home");
+      return "Working from home";
     }
+  
     getCoffeeBreak(): string {
-        throw new Error("Getting a coffee break");
+      return "Getting a coffee break";
     }
+  
     workDirectorTasks(): string {
-        throw new Error("Getting to director tasks");
+      return "Getting to director tasks";
     }
-    
-}
-
-class Teacher implements TeacherInterface{
+  }
+  
+  export class Teacher implements TeacherInterface {
     workFromHome(): string {
-        throw new Error("Cannot work from home");
+      return "Cannot work from home";
     }
     getCoffeeBreak(): string {
-        throw new Error("Cannot have a break");
+      return "Cannot have a break";
     }
     workTeacherTasks(): string {
-        throw new Error("Getting to work");
+      return "Getting to work";
     }
-    
-}
-
-function createEmployee(salary: (number | string)): (Director | Teacher) {
+  }
+  
+  export function createEmployee(salary: number | string): Teacher | Director {
     if (typeof salary === "number" && salary < 500) return new Teacher();
-
-  return new Director();
-}
-
+  
+    return new Director();
+  }
