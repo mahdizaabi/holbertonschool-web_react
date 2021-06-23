f1cef829187a361eb1fca7d9622592713f528e90
mahdizaabi <mahdi.zaabi@gmail.com>
2021-06-23 02:13:57 +0200

    add

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -15,4 +15,6 @@ interface Directors extends Teacher {
 interface printTeacherFunction {
 	(firstName: string, lastName: string): string;
 }
-const printTeacher:printTeacherFunction =  (firstName: string,lastName: string): string => `${firstName[0]}. ${lastName}`;
+const printTeacher:printTeacherFunction =  (firstName: string,lastName: string): string => `${firstName.charAt(0)}. ${lastName}`;
+
+console.log(printTeacher("mahdi", "zaabi"));
