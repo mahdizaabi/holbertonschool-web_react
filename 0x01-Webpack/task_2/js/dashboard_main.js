97015e061879e5cee014e61d3c55130100ed05e5
mahdizaabi <mahdi.zaabi@gmail.com>
2021-06-24 12:38:55 +0200

    add

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+const _ = require('lodash');
+
+$.when( $.ready ).then(function() {
+
+let counter = 0;
+function updateCounter() {
+    counter++;
+    $('#count').html(`${counter} clicks on the button`);
+};
+$('body').append($('<div>').attr('id', 'logo'));
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append($('<p>').attr('id', 'count'));
+$('body').append('<p>Copyright - Holberton School</p>');
+
+$('button').on('click', _.debounce(updateCounter, 500));
+});
