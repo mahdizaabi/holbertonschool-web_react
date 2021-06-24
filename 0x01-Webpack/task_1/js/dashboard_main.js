589bdff54a1fe0ffd1e01a2db52d1e6d2b3cc3fe
mahdizaabi <mahdi.zaabi@gmail.com>
2021-06-24 03:05:38 +0200

    add

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,6 +1,5 @@
 import $ from "jquery";
-const _ = require('lodash');
-
+import _ from ('lodash');
 
 $.when( $.ready ).then(function() {
     $('body').append('<p>Holberton Dashboard</p>');
@@ -9,8 +8,10 @@ $.when( $.ready ).then(function() {
     $('body').append($('<p>').attr('id', 'count'));
     $('body').append('<p>Copyright - Holberton School</p>');
     let counter = 0;
+    
     function updateCounter(){
-        $('#count').text(`${++counter} clicks on the button`);
+        counter++;
+        $('#count').text(`${counter} clicks on the button`);
     }
     $('button').on('click', _.debounce(updateCounter, 800))
   });
--- 0x01-Webpack/task_1/webpack.config.js
+++ 0x01-Webpack/task_1/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 module.exports = {
-  mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
+  mode: 'production',
 };
