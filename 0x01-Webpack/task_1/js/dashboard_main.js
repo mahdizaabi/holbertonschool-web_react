bb8d2c84d26a740c9194b330efc8bd90ebbd07f7
mahdizaabi <mahdi.zaabi@gmail.com>
2021-06-24 03:10:56 +0200

    ad

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,15 @@
-import $ from "jquery";
-import _ from ('lodash');
+import $ from 'jquery';
+import _ from 'lodash';
 
-$.when( $.ready ).then(function() {
-    $('body').append('<p>Holberton Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<button>Click here to get started</button>');
-    $('body').append($('<p>').attr('id', 'count'));
-    $('body').append('<p>Copyright - Holberton School</p>');
-    let counter = 0;
-    
-    function updateCounter(){
-        counter++;
-        $('#count').text(`${counter} clicks on the button`);
-    }
-    $('button').on('click', _.debounce(updateCounter, 800))
-  });
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append($('<p>').attr('id', 'count'));
+$('body').append('<p>Copyright - Holberton School</p>');
+let counter = 0;
+
+function updateCounter() {
+    counter++;
+    $('#count').html(`${counter} clicks on the button`);
+};
+$('button').on('click', _.debounce(updateCounter, 500));
--- 0x01-Webpack/task_1/webpack.config.js
+++ 0x01-Webpack/task_1/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
+
 module.exports = {
   entry: './js/dashboard_main.js',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
   },
   mode: 'production',
 };
