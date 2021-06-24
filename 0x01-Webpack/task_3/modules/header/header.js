00e2681e15c18f971d91d0f00d553d2270d86307
mahdizaabi <mahdi.zaabi@gmail.com>
2021-06-25 00:39:37 +0200

    add

--- 0x01-Webpack/task_3/modules/header/header.js
+++ 0x01-Webpack/task_3/modules/header/header.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
-
+import './header.css'
 $('body').append('<header></header>');
 $('header').append("<div id='logo'></div>");
 $('header').append('<h1>Holberton Dashboard</h1>');
