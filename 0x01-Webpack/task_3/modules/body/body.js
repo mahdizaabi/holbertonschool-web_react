bf84c8852987a84b94560b613ed616ef2cf3e0d0
mahdizaabi <mahdi.zaabi@gmail.com>
2021-06-25 00:38:31 +0200

    fixx

--- 0x01-Webpack/task_3/modules/body/body.js
+++ 0x01-Webpack/task_3/modules/body/body.js
@@ -1,5 +1,14 @@
 import _ from 'lodash';
 import $ from 'jquery';
+import './body.css';
+
+let counter = 0;
+function updateCounter() {
+    counter++;
+    $('#count').html(`${counter} clicks on the button`);
+};
+
 
 $('body').append('<button>Click here to get started</button>')
 $('body').append("<p id='count'></p>")
+$('button').on('click', _.debounce(updateCounter, 500));
