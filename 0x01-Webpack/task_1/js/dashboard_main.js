2124fee10b7db24b3e47179474f7e578dfaebf3a
mahdizaabi <mahdi.zaabi@gmail.com>
2021-06-24 02:29:17 +0200

    add

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -6,7 +6,7 @@ $.when( $.ready ).then(function() {
     $('body').append('<p>Holberton Dashboard</p>');
     $('body').append('<p>Dashboard data for the students</p>');
     $('body').append('<button>Click here to get started</button>');
-    $('body').append("<p id='count'></p>");
+    $('body').append($('<p>').attr('id', 'count'));
     $('body').append("Copyright - Holberton School");
     let counter = 0;
     function updateCounter(){
