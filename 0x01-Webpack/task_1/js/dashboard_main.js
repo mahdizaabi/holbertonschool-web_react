2aa99f5975ff24bb5aac0ee568d62b133ebb3ad9
mahdizaabi <mahdi.zaabi@gmail.com>
2021-06-24 02:59:19 +0200

    fix p element

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -7,7 +7,7 @@ $.when( $.ready ).then(function() {
     $('body').append('<p>Dashboard data for the students</p>');
     $('body').append('<button>Click here to get started</button>');
     $('body').append($('<p>').attr('id', 'count'));
-    $('body').append('Copyright - Holberton School');
+    $('body').append('<p>Copyright - Holberton School</p>');
     let counter = 0;
     function updateCounter(){
         $('#count').text(`${++counter} clicks on the button`);
