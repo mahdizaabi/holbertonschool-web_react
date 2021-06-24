7b7d26825b081c5767ca0d29fa7d3845daa64d99
mahdizaabi <mahdi.zaabi@gmail.com>
2021-06-24 03:13:42 +0200

    change lodash version

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,15 +1,19 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
+$.when( $.ready ).then(function() {
+
+let counter = 0;
+function updateCounter() {
+    counter++;
+    $('#count').html(`${counter} clicks on the button`);
+};
+
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append($('<p>').attr('id', 'count'));
 $('body').append('<p>Copyright - Holberton School</p>');
-let counter = 0;
 
-function updateCounter() {
-    counter++;
-    $('#count').html(`${counter} clicks on the button`);
-};
 $('button').on('click', _.debounce(updateCounter, 500));
+});
