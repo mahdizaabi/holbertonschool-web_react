40be96bebd22b4e5eb8853bef03cbc34a3202113
mahdizaabi <mahdi.zaabi@gmail.com>
2021-06-29 14:22:01 +0200

    task_02

---
+++ 0x02-react_intro/task_2/dashboard/src/utils.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const getFullYear = () => new Date().getFullYear();
+export const getFooterCopy = (isIndex) => isIndex ? "Holberton School" : "Holberton School main dashboard";
+
+
+
+ 
