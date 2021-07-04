1a4b9bacb629cbde678c4cac741cded36b27d7b7
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-04 23:42:19 +0200

    add

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,69 +1,62 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: '../src/index.js',
-    output: {
-        path: path.join(__dirname, '..', 'dist'),
-        filename: 'bundle.js'
-    },
-    mode: 'production',
-    performance: {
-        maxAssetSize: 100000,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                    'file-loader',
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            bypassOnDebug: true, // webpack@1.x
-                            disable: true, // webpack@2.x and newer
-                        },
-                    },
-                ],
-            }, {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/react']
-                    }
-                }
-            }, {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/react']
-                    }
-                }
-            }
-        ],
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-        },
-    },
-    devtool: 'source-map',
-    plugins: [
-        new HtmlWebpackPlugin(),
-        new CleanWebpackPlugin()],
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        compress: true,
-        port: 8564,
-        hot:true
-    },
+	context: path.resolve(__dirname, '../src'),
+	entry: './index.js',
+	output: {
+		path: path.resolve('./public'),
+		filename: 'bundle.js',
+		clean: true,
+	},
+	mode: 'development',
+	devServer: {
+		contentBase: path.resolve('../dist/'),
+		compress: true,
+		port: 8564,
+		hot: true,
+	},
+	devtool: 'inline-source-map',
+	performance: {
+    maxAssetSize: 100000,
+  },
+	module: {
+    rules: [
+			{
+				test: /\.css$/, use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				use: [
+					'file-loader',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassOnDebug: true, // webpack@1.x
+							disable: true, // webpack@2.x and newer
+						},
+					},
+				],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/react']
+					}
+				}
+			},
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/react']
+					}
+				}
+			},
+    ],
+	},
 }
