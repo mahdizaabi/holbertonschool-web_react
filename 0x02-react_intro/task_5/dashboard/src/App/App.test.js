d56bafd13faeb6b2eb247c6144a8449fd27a0a57
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-07 01:24:54 +0200

    set configuration for image loading

--- 0x02-react_intro/task_5/dashboard/src/App/App.test.js
+++ 0x02-react_intro/task_5/dashboard/src/App/App.test.js
@@ -3,20 +3,20 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 describe('<App />', () => {
-  it('App renders without crashing', () => {
+  it('<App /> renders without crashing', () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toHaveLength(1);
 	});
-
-    it('App renders a div with the class App-header', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find('div.App-header')).toHaveLength(1);
-    });
-    it('App renders a div with the class App-body', () => {
+    it('<App /> renders a div with the class App-body', () => {
         const wrapper = shallow(<App />);
         expect(wrapper.find('div.App-body')).toHaveLength(1);
     });
-    it('App renders a div with the class App-footer', () => {
+    it('<App /> renders a div with the class App-header', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('div.App-header')).toHaveLength(1);
+    });
+    
+    it('<App /> have a div with the class App-footer', () => {
         const wrapper = shallow(<App />);
         expect(wrapper.find('div.App-footer')).toHaveLength(1);
     });
