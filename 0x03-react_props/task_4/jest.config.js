ee43b63b1f5001bc359f965a8403d1739e97e371
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-06 12:15:00 +0200

    refactor folder

--- 0x03-react_props/task_4/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-    setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
-    moduleNameMapper: {
-        '\\.(css|less|png|jpg)$': '<rootDir>/styleMock.js',
-      }
- 
-};
--- 0x03-react_props/task_4/src/utils.js
+++
@@ -1,9 +0,0 @@
-
-export const getFullYear = () => new Date().getFullYear();
-export const getFooterCopy = (isIndex) => isIndex ? "Holberton School" : "Holberton School main dashboard";
-export const getLatestNotification = () =>{ return {__html: '<strong>Urgent requirement</strong> - complete by EOD'}};
-
-
-
-
- 
