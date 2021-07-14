84d38c486fd1f69af7939a4dbd4c65369b496de3
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-14 04:48:18 +0200

    add

---
+++ 0x04-React_component/task_5/dashboard/src/BodySection/BodySection.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import BodySectionWithMarginBottom from './BodySectionWithMarginBottom';
+import BodySection from './BodySection';
+
+
+describe("<BodySectionWithMarginBottom />", () => {
+    it('render correctly the child component', () => {
+        const wrapper = shallow(<BodySectionWithMarginBottom title="test">
+            <BodySection />
+            </BodySectionWithMarginBottom>);
+            expect(wrapper.containsMatchingElement(<BodySection/>)).toBeTruthy()
+    })
+    it('Called the child component with the right arguments', () => {
+        const wrapper = shallow(<BodySectionWithMarginBottom title="test">
+            <BodySection />
+            </BodySectionWithMarginBottom>);
+            expect(wrapper.childAt(0).props().title).toBeTruthy();
+            expect(wrapper.childAt(0).props().title).toBe('test');
+    })
+    
+   
+   
+})
