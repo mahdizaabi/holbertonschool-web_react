883d9931a91bdbbfb4139ad6886327af31e36a0a
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-22 14:53:51 +0200

    add

--- 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 import './CourseList.css'
 
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
+    let rowColor = isHeader ? {"backgroundColor":"#f5f5f5ab"} : {"backgroundColor":"#deb5b545"}
     return (
-        <tr className="row" style={{"backgroundColor":"#f5f5f5ab"}}>
-            {isHeader && !textSecondCell && <th syle={{"backgroundColor":"#deb5b545"}} colSpan = "2">{textFirstCell}</th>}
-            {isHeader && textSecondCell && (<><th syle={{"backgroundColor":"#deb5b545"}}>{textFirstCell}</th><th syle={{"backgroundColor":"#deb5b545"}}>{textSecondCell}</th></>)}
-            {!isHeader && (<> <td>{textFirstCell}</td><td>{textSecondCell}</td> </>)}</tr>
+        <tr className="row" >
+            {isHeader && !textSecondCell && <th syle={rowColor} colSpan = "2">{textFirstCell}</th>}
+            {isHeader && textSecondCell && (<><th syle={rowColor}>{textFirstCell}</th><th syle={rowColor}>{textSecondCell}</th></>)}
+            {!isHeader && (<> <td style={rowColor}>{textFirstCell}</td><td style={rowColor}>{textSecondCell}</td> </>)}</tr>
     )
 }
 
