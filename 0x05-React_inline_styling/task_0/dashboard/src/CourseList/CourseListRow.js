c6ccff63d0c4254849c2398a433470c27b779729
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-22 14:44:35 +0200

    add

--- 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -6,7 +6,7 @@ const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
     return (
         <tr className="row" style={{"backgroundColor":"#f5f5f5ab"}}>
             {isHeader && !textSecondCell && <th syle={{"backgroundColor":"#deb5b545"}} colSpan = "2">{textFirstCell}</th>}
-            {isHeader && textSecondCell && (<><th syle={{"backgroundColor":"#deb5b545"}}>{textFirstCell}</th><th>{textSecondCell}</th></>)}
+            {isHeader && textSecondCell && (<><th syle={{"backgroundColor":"#deb5b545"}}>{textFirstCell}</th><th syle={{"backgroundColor":"#deb5b545"}}>{textSecondCell}</th></>)}
             {!isHeader && (<> <td>{textFirstCell}</td><td>{textSecondCell}</td> </>)}</tr>
     )
 }
