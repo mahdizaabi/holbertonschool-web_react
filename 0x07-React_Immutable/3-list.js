e8465d20f776db9f3b85c7481749c688824c8517
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-28 04:49:54 +0200

    add

---
+++ 0x07-React_Immutable/3-list.js
@@ -0,0 +1,9 @@
+const { List } = require('immutable');
+
+export function getListObject(array) {
+  return List(array);
+}
+
+export function addElementToList(list, element) {
+  return list.push(element);
+}
