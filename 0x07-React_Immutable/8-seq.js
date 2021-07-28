cdfa188f5d28daab8c7cfaf938a2fccad3b20a7c
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-28 21:38:32 +0200

    seq

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -3,12 +3,11 @@ import { Seq } from 'immutable';
 const printBestStudents = (students) => {
   const filteredStudents = Seq(students)
     .filter((student) => student.score >= 70)
-    .map((studentObj) => {
-      const student = studentObj;
-      student.firstName = student.firstName[0].toUpperCase() + student.firstName.slice(1);
-      student.lastName = student.lastName[0].toUpperCase() + student.lastName.slice(1);
-      return student;
-    }).toObject();
+    .map((filtredStudent) => ({
+      ...filtredStudent,
+      firstName: student.firstName[0].toUpperCase() + student.firstName.slice(1),
+      lastName: student.lastName[0].toUpperCase() + student.lastName.slice(1),
+    })).toObject();
 
   console.log(filteredStudents);
 };
